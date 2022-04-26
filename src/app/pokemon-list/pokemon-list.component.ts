04fc079a9335fdf45719d6435b963b887a6c09af
Maykon Taroco <maykontaroco@gmail.com>
2022-04-26 10:26:17 -0300

    First commit

---
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+
+@Component({
+  selector: 'app-pokemon-list',
+  templateUrl: './pokemon-list.component.html',
+  styleUrls: ['./pokemon-list.component.sass']
+})
+export class PokemonListComponent implements OnInit {
+  ngOnInit(){} void;
+
+  constructor(public pokemonService: PokemonService) {}
+}
